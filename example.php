1c355a7d6029260efa60b8132e5bf149e722d37b
Ilya Shapkin <i.m.shapkin@yandex.ru>
2017-12-07 09:29:33 +0300

    Kill auth data

--- example.php
+++ example.php
@@ -1,5 +1,5 @@
 <?php
 require __DIR__ . DIRECTORY_SEPARATOR;
 use \Coursarium\OpenWindow\Auth as OpenWindowAuth;
-$ow = new OpenWindowAuth("universarium ", "3kplctd2");
+$ow = new OpenWindowAuth("username", "password");
 var_export($ow->getUserInfo());
